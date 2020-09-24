e5f1c8a9cf950642a6b270bb9abdffee1741ff52
lyj00912 <3122742760@qq.com>
2020-09-24 21:23:32 +0800

    created P3_DeltaTimeTest.cs to move the cube on a Quadratic Function

---
+++ Assets/1.Components/P3_DeltaTimeTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class P3_DeltaTimeTest : MonoBehaviour {
+
+	private float NowXPosition = 0f;
+	private float Step = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		NowXPosition += Step * Time.deltaTime;
+		if (NowXPosition >= 1.5f) {
+			Step = -1f;
+		} else if (NowXPosition <= -1.5f) {
+			Step = 1f;
+		}
+
+		float NowYPosition = -1 * (NowXPosition - 0f) * (NowXPosition - 0f) + 3f;
+
+		Transform t = gameObject.GetComponent<Transform> ();
+		t.position = new Vector3(NowXPosition, NowYPosition, 1.5f);
+	}
+}

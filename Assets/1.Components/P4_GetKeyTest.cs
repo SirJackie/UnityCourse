70682f1c71606346f7230c6e44e5f817a51c0f49
lyj00912 <3122742760@qq.com>
2020-09-24 21:59:17 +0800

    created Keyboard Control by creating P4_GetKeyTest.cs

---
+++ Assets/1.Components/P4_GetKeyTest.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class P4_GetKeyTest : MonoBehaviour {
+
+	public float Speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float DeltaX = 0f;
+		float DeltaY = 0f;
+		float DeltaZ = 0f;
+
+		if (Input.GetKey (KeyCode.W)) {
+			DeltaZ -= Speed;
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			DeltaZ += Speed;
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			DeltaX += Speed;
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			DeltaX -= Speed;
+		}
+
+		Transform t = gameObject.GetComponent<Transform> ();
+		var DeltaPosition =  new Vector3 (
+			DeltaX,
+			DeltaY,
+			DeltaZ
+		);
+		DeltaPosition.Normalize ();
+		DeltaPosition *= Speed;
+		t.position += DeltaPosition;
+
+	}
+}

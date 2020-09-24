0665c4fab60c019226bf3e514afae545a3920035
lyj00912 <3122742760@qq.com>
2020-09-24 21:05:29 +0800

    created P2_LifeCycle.cs

---
+++ Assets/1.Components/P2_LifeCycle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class P2_LifeCycle : MonoBehaviour {
+
+	void Awake () {
+		Debug.Log ("Awake");
+	}
+
+	void OnEnable () {
+		Debug.Log ("OnEnable");
+	}
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Debug.Log ("Update");
+	}
+
+	void OnDisable () {
+		Debug.Log ("OnDisable");
+	}
+
+	void OnDestroy () {
+		Debug.Log ("OnDestroy");
+	}
+}

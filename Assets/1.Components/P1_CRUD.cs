ae543a7cbf2d3b184f81b2e77ff1f970489d9703
lyj00912 <3122742760@qq.com>
2020-09-24 20:45:55 +0800

    created P1_CRUD.cs and modified .gitignore

---
+++ Assets/1.Components/P1_CRUD.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class P1_CRUD : MonoBehaviour {
+
+	// Options
+	public bool GetComponent;
+	public bool AddComponent;
+	public bool UpdateTransformComponent;
+	public bool DeleteRendererComponent;
+
+	// Use this for initialization
+	void Start () {
+
+		// Get Component
+		if (GetComponent == true) {
+			Component c = gameObject.GetComponent<P1_CRUD> ();
+			Debug.Log (c);
+		}
+
+		// Add Component
+		if (AddComponent == true) {
+			gameObject.AddComponent<EmptyComponent> ();
+		}
+
+		// Update Transform Component
+		if (UpdateTransformComponent == true) {
+			Transform t = gameObject.GetComponent<Transform> ();
+			t.position = new Vector3 (1.5f, 1.5f, 1.5f);
+		}
+
+		// Delete Renderer Component
+		if (DeleteRendererComponent == true) {
+			Renderer r = gameObject.GetComponent<Renderer> ();
+			Destroy (r);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// Update Transform Component
+		if (UpdateTransformComponent == true) {
+			Transform t = gameObject.GetComponent<Transform> ();
+			t.position = new Vector3 (1.5f, 1.5f, 1.5f);
+		} else {
+			Transform t = gameObject.GetComponent<Transform> ();
+			t.position = new Vector3 (1.5f, 1.0f, 1.5f);
+		}
+
+	}
+}
